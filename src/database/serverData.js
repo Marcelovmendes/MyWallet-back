738353e5d103887eae268a9e4a46699b7136e464
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-09-11 15:36:48 -0300

    feat: connection deploy

--- src/database/serverData.js
+++ src/database/serverData.js
@@ -2,17 +2,16 @@ import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 dotenv.config();
 
-/*const mongoClient = new MongoClient(process.env.DATABASE_URL);
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
-    await mongoClient.connect();
-  } catch (err) {
-    console.log(err.message);
-  }
-  export const db = mongoClient.db();
-*/
+  await mongoClient.connect();
+} catch (err) {
+  console.log(err.message);
+}
+export const db = mongoClient.db();
 
-const mongoClient = new MongoClient('mongodb://localhost:27017/test');
+/*const mongoClient = new MongoClient('mongodb://localhost:27017/test');
 export let db;
 
-mongoClient.connect().then(() => (db = mongoClient.db()));
+mongoClient.connect().then(() => (db = mongoClient.db())); */
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from './app.js';
 //process.env.PORT
-app.listen(5000, () => console.log('On Server'));
+app.listen(process.env.PORT, () => console.log('On Server'));
